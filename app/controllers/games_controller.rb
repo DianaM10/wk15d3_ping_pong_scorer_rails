999d593e642bc487a7454d802129f42a2d363b10
Diana Man <dianaman82@gmail.com>
2017-01-18 12:04:18 +0000

    added team a player 1 nick name

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,12 @@ class GamesController < ApplicationController
 
   def index
     games = Game.all
-    render json: games 
+    render json: games.as_json({
+      include: [
+        { team_a_player1: {only: :nickname } }
+      ]
+    })
+    
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :team_a_player1
-  belongs_to :team_b_player1
+  belongs_to :team_a_player1, class_name: 'Player'
+  belongs_to :team_b_player1, class_name: 'Player'
 end
