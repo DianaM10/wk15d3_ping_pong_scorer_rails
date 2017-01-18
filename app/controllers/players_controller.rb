a775b1a324fd7cf1d19a5b88ee3b1bc207231a97
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-18 11:31:03 +0000

    only returning nickname from players index route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,11 @@ class PlayersController < ApplicationController
   before_action :authenticate_user!
   def index
     players = Player.all
-    render json: players 
+    render json: players.as_json(
+      only: [
+        :nickname
+      ]
+    )
   end
 
 end
